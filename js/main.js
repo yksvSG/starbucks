188e8ecbc7f58e7996b5149e8d697ffbe549d9dd
yksvSG <sg.yksv77@gmail.com>
2023-01-04 17:52:38 +0900

    Start project

---
+++ js/main.js
@@ -0,0 +1,163 @@
+// 1. searchEl 를 클릭시 searchInputEl에 focus되는 이벤트 핸들러를 등록
+// 2. searchInputEl 가 focus 되면,
+//     2.1. searchEl 의 속성으로 focused 를 추가하고,
+//     2.2. searchInputEl를 선택하여, placeholder 속성의 값으로 '통합검색'을 지정한다.
+// 3. searchInputEl 가 blur 되면,
+//     3.1. searchEl 의 속성인 focused 를 제거하고,
+//     3.2. searchInputEl를 선택하여, placeholder 속성의 값으로 ''을 지정한다.
+
+const searchEl = document.querySelector(".search");
+const searchInputEl = searchEl.querySelector("input");
+
+searchEl.addEventListener("click", () => {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener("focus", () => {
+  searchEl.classList.add("focused");
+  searchInputEl.setAttribute("placeholder", "통합검색");
+});
+
+searchInputEl.addEventListener("blur", () => {
+  searchEl.classList.remove("focused");
+  searchInputEl.setAttribute("placeholder", "");
+});
+
+const badgeEl = document.querySelector("header .badges");
+const toTopEl = document.querySelector("#to-top");
+// window 객체는 현재 보고있는 화면 자체이다.
+// .throttle(함수, 시간)
+window.addEventListener(
+  "scroll",
+  _.throttle(() => {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+      gsap.to(toTopEl, 0.2, {
+        x: 0,
+      });
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+      gsap.to(toTopEl, 0.2, {
+        x: 100,
+      });
+    }
+  }, 300)
+);
+
+toTopEl.addEventListener("click", () => {
+  gsap.to(window, 0.7, {
+    scrollTo: 0,
+  });
+});
+
+// 순차적 애니메이션: 순차적으로 요소 보이기
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+fadeEls.forEach((fadeEl, index) => {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1,
+  });
+});
+
+new Swiper(".notice-line .swiper-container", {
+  direction: "vertical",
+  autoplay: true,
+  // loop: 반복재생여부
+  loop: true,
+});
+
+new Swiper(".promotion .swiper-container", {
+  // direction: 'horizontal' 수평 이동을 기본값으로 한다.
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  // autoplay: {
+  //   delay: 5000,
+  // },
+  pagination: {
+    el: ".promotion .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
+});
+
+new Swiper(".awards .swiper-container", {
+  // direction: 'horizontal', // 수평 슬라이드
+  autoplay: true, // 자동 재생 여부
+  loop: true, // 반복 재생 여부
+  spaceBetween: 30, // 슬라이드 사이 여백
+  slidesPerView: 5, // 한 번에 보여줄 슬라이드 개수
+  navigation: {
+    prevEl: ".awards .swiper-prev",
+    nextEl: ".awards .swiper-next",
+  },
+});
+
+const promotionEl = document.querySelector(".promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promotion");
+let isHidePromotion = false;
+// 1. promotionToggleBtn 을 클릭하면, promotionEl가 열고 닫는 이벤트를 등록한다.
+// 2. isHidePromotion이 true 라면, promotionEl의 클래스에 hide를 추가하고
+// 3. isHidePromotion 이 false 라면, promotionEl의 클래스에 hide를 제거한다.
+promotionToggleBtn.addEventListener("click", () => {
+  // 클릭할 때마다, 열고 닫을 수 있는 첫번째 조건을 설정
+  isHidePromotion = !isHidePromotion;
+
+  if (isHidePromotion) {
+    promotionEl.classList.add("hide");
+  } else {
+    promotionEl.classList.remove("hide");
+  }
+});
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2));
+}
+
+function floatingObject(selector, delay, size) {
+  gsap.to(selector, 1, {
+    y: size,
+    repeat: -1,
+    yoyo: true,
+    ease: Power1.easeInOut,
+    delay: random(0, delay),
+  });
+}
+floatingObject(".floating1", 1, 15);
+floatingObject(".floating2", 0.5, 15);
+floatingObject(".floating3", 1.5, 20);
+
+/**
+ * ScrollMagic 를 사용해서 특정 section이 보이는지에 따라, 원하는 클래스를 추가한다.
+ * 추가된 클래스를 사용하여 CSS 처리를 할 수 있게 된다.
+ * 요소가 화면에 보여짐 여부에 따른 요소 관리
+ */
+// 관리할 요소들 검색!
+const spyEls = document.querySelectorAll("section.scroll-spy");
+// 요소들 반복 처리!
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic.Scene({
+    // 감시할 장면(Scene)을 추가
+    triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+    triggerHook: 0.8, // 화면의 80% 지점에서 보여짐 여부 감시
+  })
+    .setClassToggle(spyEl, "show") // 요소가 화면에 보이면 show 클래스 추가
+    .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수!)
+});
+
+const thisYear = document.querySelector(".this-year");
+thisYear.textContent = new Date().getFullYear();
---
+++ js/youtube.js
@@ -0,0 +1,28 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement("script");
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+// onYouTubeIframeAPIReady 함수 이름은 변경 불가!
+// 외부에서 가져온 유뷰트 라이브러리가 자동으로 onYouTubeIframeAPIReady 함수를 찾아서 실행하므로 이름을 변경해서는 안됨
+
+function onYouTubeIframeAPIReady() {
+  new YT.Player("player", {
+    videoId: "An6LvWQuj_8", // 최초 재생할 유튜브 영상 ID
+    playerVars: {
+      autoplay: true,
+      loop: true, // 반복 재생 유뮤 ** 필수조건 : 반복재생할 유튜브 영상 ID 목록을 재공해야함
+      playlist: "An6LvWQuj_8", // 반복재생할 유튜브 영상 ID 목록
+    },
+    events: {
+      onReady: (event) => {
+        // event.target.mute(); // 음소거
+        event.target.playVideo(); // 음소서 없이 재생
+      },
+    },
+  });
+}
