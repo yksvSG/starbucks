4c0693ecba74cbe72743d165d3eede611fee763a
yksvSG <sg.yksv77@gmail.com>
2023-01-04 20:29:33 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
-// 1. searchEl 를 클릭시 searchInputEl에 focus되는 이벤트 핸들러를 등록
-// 2. searchInputEl 가 focus 되면,
-//     2.1. searchEl 의 속성으로 focused 를 추가하고,
-//     2.2. searchInputEl를 선택하여, placeholder 속성의 값으로 '통합검색'을 지정한다.
-// 3. searchInputEl 가 blur 되면,
-//     3.1. searchEl 의 속성인 focused 를 제거하고,
-//     3.2. searchInputEl를 선택하여, placeholder 속성의 값으로 ''을 지정한다.
-
-const searchEl = document.querySelector(".search");
-const searchInputEl = searchEl.querySelector("input");
-
-searchEl.addEventListener("click", () => {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener("focus", () => {
-  searchEl.classList.add("focused");
-  searchInputEl.setAttribute("placeholder", "통합검색");
-});
-
-searchInputEl.addEventListener("blur", () => {
-  searchEl.classList.remove("focused");
-  searchInputEl.setAttribute("placeholder", "");
-});
-
 const badgeEl = document.querySelector("header .badges");
 const toTopEl = document.querySelector("#to-top");
 // window 객체는 현재 보고있는 화면 자체이다.
@@ -158,6 +133,3 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, "show") // 요소가 화면에 보이면 show 클래스 추가
     .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수!)
 });
-
-const thisYear = document.querySelector(".this-year");
-thisYear.textContent = new Date().getFullYear();
